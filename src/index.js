24c82be8d5f047e5276a29c014ca881a555a5c05
OG-Luffy <luckysingh87753@gmail.com>
2026-09-21 13:59:29 +0530

    Enable voice state intent for EMOX voice channel joining

--- src/index.js
+++ src/index.js
@@ -36,6 +36,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildVoiceStates,
     GatewayIntentBits.MessageContent
   ]
 });
